8bb2fbc251ed2d6a720221d8200a5e08cc947fb2
Burak-A-dev <alpay.ozdemirok@gmail.com>
2026-05-20 00:10:06 +0200

    domain entites + application dtos created

---
+++ Application/DTOs/LiveSessionDetailDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Lms.LiveClassService.Application.DTOs;
+
+public class LiveSessionDetailDto
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string HostName { get; set; } = string.Empty;
+    public string VideoUrl { get; set; } = string.Empty;
+    public List<ViewerDto> Viewers { get; set; } = new();
+}
---
+++ Domain/Entities/LiveSession.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lms.LiveClassService.Domain.Entities
+{
+    public class LiveSession
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string HostName { get; set; } = string.Empty;
+        public string VideoUrl { get; set; } = string.Empty;
+        public List<Viewer> Viewers { get; set; } = new();
+    }
+}
