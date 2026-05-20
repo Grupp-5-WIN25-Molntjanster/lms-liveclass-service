fe4ed8ee1dce8d5b6c600618cc7af5e2f79126f2
Burak-A-dev <alpay.ozdemirok@gmail.com>
2026-05-20 23:25:08 +0200

    LiveSession interface and repository created

---
+++ Application/Interfaces/ILiveSessionRepository.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+using Lms.LiveClassService.Domain.Entities;
+
+namespace Lms.LiveClassService.Application.Interfaces;
+
+public interface ILiveSessionRepository
+{
+    Task<LiveSession> GetActiveSessionAsync();
+}
---
+++ Infrastucture/Repositories/LiveSessionMockRepository.cs
@@ -0,0 +1,27 @@
+﻿using Lms.LiveClassService.Application.Interfaces;
+using Lms.LiveClassService.Domain.Entities;
+
+namespace Lms.LiveClassService.Infrastructure.Repositories;
+
+public class LiveSessionMockRepository : ILiveSessionRepository
+{
+    public Task<LiveSession> GetActiveSessionAsync()
+    {
+        var session = new LiveSession
+        {
+            Id = Guid.NewGuid(),
+            Title = "Foundations of Digital Marketing",
+            HostName = "John Does",
+            VideoUrl = "",
+            Viewers = new List<Viewer>
+            {
+                new Viewer { Id = Guid.NewGuid(), Name = "Student 1", AvatarUrl = "" },
+                new Viewer { Id = Guid.NewGuid(), Name = "Student 2", AvatarUrl = "" },
+                new Viewer { Id = Guid.NewGuid(), Name = "Student 3", AvatarUrl = "" },
+                new Viewer { Id = Guid.NewGuid(), Name = "Student 4", AvatarUrl = "" }
+            }
+        };
+
+        return Task.FromResult(session);
+    }
+}
